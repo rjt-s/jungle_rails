8b15832473756a6bb2900e23f59cfca18200f8e9
Rajat Sharma <srma.rjt@gmail.com>
2023-02-03 19:20:25 -0500

    cypress test added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,18 +7,36 @@ RSpec.describe Product, type: :model do
   
   it 'should create a new product' do
     category = Category.create(name:'berries')
-    product.create(name:'AloeVira',price: 30, quantity: 22, category: category)
+    product = Product.create(name:'AloeVira',price: 30, quantity: 22, category: category)
     expect(product).to be_valid
   end
 
-  # it 'should have a name' do
-  #   @product = Product.new
-  #   expect(@product.name).to be_present
-  # end
+  it 'should not create a product without a name' do
+    category = Category.create(name:'berries')
+    product = Product.create(name: nil,price: 30, quantity: 22, category: category)
+    expect(product).to_not be_valid
+    expect(product.errors.full_messages).to contain_exactly("Name can't be blank")
+  end
 
-  # it 'should have a price' do
-  #   @product = Product.price
-  #   expect(@product.price).to be_present 
-  # end
+  it 'should not create a product without a price' do
+    category = Category.create(name:'berries')
+    product = Product.create(name: 'AloeVera',price_cents: nil, quantity: 22, category: category)
+    expect(product).to_not be_valid
+    expect(product.errors.full_messages).to contain_exactly("Price can't be blank", "Price cents is not a number", "Price is not a number")
+  end
+
+  it 'should not create a product without a quantity' do
+    category = Category.create(name:'berries')
+    product = Product.create(name: 'AloeVera',price: 30, quantity: nil, category: category)
+    expect(product).to_not be_valid
+    expect(product.errors.full_messages).to contain_exactly("Quantity can't be blank")
+  end
+
+  it 'should not create a product without a category' do
+    product = Product.create(name: 'AloeVera',price: 30, quantity: 22, category: nil)
+    expect(product).to_not be_valid
+    expect(product.errors.full_messages).to contain_exactly("Category can't be blank", "Category must exist")
+  end
+
  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+  # validation tests/examples here
+    it 'should not create a User without a password' do
+      user = User.create(name: nil,price: 30, quantity: 22, category: category)
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to contain_exactly("Password can't be blank")
+    end
+  end
 end
