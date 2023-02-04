49de86e158f2b0a62bc18b2a5ccbaa848053e152
Rajat Sharma <srma.rjt@gmail.com>
2023-02-04 14:25:01 -0500

    user spec and validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :first_name, presence:true
+  validates :last_name,presence:true
+  validates :email,presence:true, uniqueness: { case_sensitive: false }
+  validates :password,presence:true, length: { minimum: 8 }
+  validates :password_confirmation, presence:true
+
+  def self.authenticate_with_credentials(email, password)
+    # how to ensure if user is auntenticated
+    # trim and lowecase the email
+    user = User.find_by_email(email.strip.downcase)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,43 @@ RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe 'Validations' do
   # validation tests/examples here
-    it 'should not create a User without a password' do
-      user = User.create(name: nil,price: 30, quantity: 22, category: category)
+    it 'should not create a User without a first name' do
+      user = User.create(first_name: nil,last_name: 'Sharma', email: 'rajatgn@gmail.com', password: 'abc', 
+      password_confirmation:'abc')
       expect(user).to_not be_valid
-      expect(user.errors.full_messages).to contain_exactly("Password can't be blank")
+      expect(user.errors.full_messages).to contain_exactly("First name can't be blank", "Password is too short (minimum is 8 characters)")
     end
+   
+    it 'should not create a User without a last name' do
+      user = User.create(first_name: 'Rajat',last_name: nil, email: 'rajatgn@gmail.com', password: 'abc', 
+      password_confirmation:'abc')
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to contain_exactly("Last name can't be blank", "Password is too short (minimum is 8 characters)")
+    end
+
+    it 'should not create a User without a email' do
+      user = User.create(first_name: 'Rajat',last_name: 'Sharma', email: nil, password: 'abc', 
+      password_confirmation:'abc')
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to contain_exactly("Email can't be blank", "Password is too short (minimum is 8 characters)")
+    end
+
+    it 'should not create a User without a password' do
+      user = User.create(first_name: 'Rajat',last_name: 'Sharma', email: 'rajatgn@gmail.com', password: nil, 
+      password_confirmation:'abc')
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to contain_exactly("Password can't be blank","Password can't be blank","Password is too short (minimum is 8 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+    # it 'should not create a User without a password' do
+    #   user = User.create(first_name: 'Rajat',last_name: 'Sharma', email: 'rajatgn@gmail.com', password: nil, 
+    #   password_confirmation:'abc')
+    #   expect(user).to_not be_valid
+    #   expect(user.errors.full_messages).to contain_exactly("Password can't be blank")
+    # end
+
   end
 end
